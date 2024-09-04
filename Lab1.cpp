802445cac5dfc804c8a7f6bdd293ff8d280a5a02
Илья Горюнов <ilyagorunov.04@gmail.com>
2024-09-04 10:18:48 +0300

    Подготовка к созданию потоков

--- Lab1.cpp
+++ Lab1.cpp
@@ -13,7 +13,7 @@ int** generateMatrix(int rows, int columns) {
     for (int i = 0; i < rows; i++) {
         int* array = new int[columns];
         for (int j = 0; j < columns; j++) {
-            array[j] = rand() % 1000 + 1;
+            array[j] = rand() % 10 + 1;
         }
         mas[i] = array;
     }
@@ -29,14 +29,41 @@ void printMatrix(int** mas, int rows, int columns) {
     }
 }
 
+int countOnes(int num) {
+    int cnt = 0;
+    while (num > 0) {
+        cnt += num % 2;
+        num = num / 2;
+    }
+    return cnt;
+}
+
 int main()
 {
     srand(time(NULL));
+    // generate parameters
     int M = generateInt();
     int N = generateInt();
     cout << M << " " << N << endl;
+
     int** matrix = generateMatrix(M, N);
+
+    // count ones in binary
+    int result = 0;
+    for (int k = 0; k < M; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                result += countOnes(matrix[k][i] * matrix[k][j]);
+            }
+        }
+
+    }
+
+
+    // printing
     printMatrix(matrix, M, N);
+
+    cout << result << endl;
     return 0;
 
 }
